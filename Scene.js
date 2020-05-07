e112352556128193c7661afd2dccf646e897f6ce
PaDiOc18 <50026864+PaDiOc18@users.noreply.github.com>
2020-05-06 20:47:16 -0700

    Update Scene.js

--- Scene.js
+++ Scene.js
@@ -1,48 +1,91 @@
 let scene = null,
-    window_relation = window.innerWidth / window.innerHeight;
+    window_relation = window.innerWidth / window.innerHeight
+    camera = null,
+    player = true,
+    camera_control = null,
+    renderer = null;
+
+function EraseSnake(){
+    snake.snakeClear();
+};
+
+function ChangeModePlayer(){
+    EraseSnake();
+    player = true;
+    camera.position.set( -1.3, 0, -4);
+    camera.rotation.y = 180 * (Math.PI /180);
+    camera.rotation.x = 10 * (Math.PI / 180);
+    camera.rotation.z = 0 * (Math.PI / 180);
+    camera_control.enabled = false;
+};
+
+function ChangeModeAI(){
+    EraseSnake();
+    player = false;
+    camera.position.set( 3.7, 3.7, 3.7 );
+    //camera.rotation.y = -180 * (Math.PI /180);
+    //camera.rotation.x = -20 * (Math.PI / 180);
+    camera_control = new THREE.OrbitControls( camera,renderer.domElement);
+    camera_control.enabled = true;
+
+};
 
 
 window.addEventListener('load',()=>{
-    scene = new THREE.Scene(); //Creamos la escena 
-    let renderer = new THREE.WebGLRenderer({ antialias: true, alpha:true }), //Creamos el renderizador
-        canvas = document.getElementById('canvas'), //Llamamos el canvas del HTML
-        camera = new THREE.PerspectiveCamera( 75, window_relation, 0.1, 1000 ); //Creamos la cámara 
-    let camera_direction = [0,0,0], //La dirección a la que la cámara apunta
-        cameraValidDirection = [false,false,false], //Válida si la dirección a la que apunta si hay algo que mostrar
-        radiansDirection = [0,0,0]; //Dirección en radianes de cada posicion
-    camera.position.set( 6, 6, 6 ); //Asignamos la posicion de la camara 
-	camera.rotation.y = Math.PI / 4; //Rotamos la camara en Y 45 grados
-	camera.rotation.x = Math.atan( - 1 / Math.sqrt( 2 ) ); //Rotamos la cámara en x para la perspectica Isómetrica
+    //let rx = document.getElementById('rx');
+    scene = new THREE.Scene();
+    renderer = new THREE.WebGLRenderer({ antialias: true, alpha:true });
+    let canvas = document.getElementById('canvas'),
+        frame_relation = canvas.offsetWidth/canvas.offsetHeight;
 
+        camera = new THREE.PerspectiveCamera( 75, frame_relation, 0.1, 1000 );
+    
+    //Isometric View
+    camera.position.set( -1.3, 0, -4);
+    camera.rotation.y = 180 * (Math.PI /180);
+	camera.rotation.x = 20 * (Math.PI / 180);
 
-    renderer.setSize( window.innerWidth, window.innerHeight ); //Asignamos el tamaño del renderizador
-    canvas.appendChild( renderer.domElement ); //Y lo agregamos al canvas
+    canvas.appendChild( renderer.domElement );
+    renderer.setClearColor( 0x000000, 0 );
+    renderer.setSize(canvas.offsetWidth,canvas.offsetHeight);
 
-    renderer.setClearColor( 0x000000, 0 ); //Le agregamos color blanco
-    renderer.setSize(canvas.offsetWidth,canvas.offsetHeight); //La compensación de la cámara
+    scene.add(snake.body[0]);
+    scene.add(apple.object);
+    window.addMatrix3DToElement(cell,scene);
 
-    let camera_control = new THREE.OrbitControls( camera,renderer.domElement); //Asignamos el manejo de la función OrbitControls
-    scene.add(snake.body[0]);//Agregamos cabeza de snake a la escena            //Para movernos en torno al espacio  
-    scene.add(apple.object); //Agregamos la manzana 
-    window.addCellsToScene(cell); //Y agregamos las celdas
+    scene.position.set(-(x-1)/2,-(y-1)/2,-(z-1)/2);
 
-    var axesHelper = new THREE.AxesHelper( 5 ); //El AxisWorld que nos sirve de referencia
-    axesHelper.size = 10; //Tamaño del Eje
-    scene.add( axesHelper ); //Lo agregamos a la escena 
+    var axesHelper = new THREE.AxesHelper( 5 );
+    axesHelper.size = 10;
+    scene.add( axesHelper );
+
+    function UpdateOno(){
+        if(player == false){
+            camera_control.update();
+        }
+
+    }
 
     let animate = function () {
         requestAnimationFrame(animate);
         snake.move();
         renderer.render( scene, camera );
+        UpdateOno();
     };
+
+
     animate();
-
-/*Snake Listener*/
+    /*Snake Listener*/
     let keyCode;
-    document.addEventListener("keydown", (event)=>{ //La ventana espera en caso de que presionemos una tecla 
-             keyCode = event.key;
-             snake.changeDirection(keyCode); //Se lo enviamos a snake para el cambio de dirección 
-           });
+    document.addEventListener("keydown", (event)=>{
+         keyCode = event.key;
+         snake.changeDirection(keyCode);
+       });
 
+    document.getElementById('auto-rotation').addEventListener("change", element =>{
+        if(player == false){
+            camera_control.autoRotate = element.target.checked;
+        }
+    })
 });
 
